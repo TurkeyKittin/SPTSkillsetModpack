ba2bf49773f38f9fbe552037e5ba46b9b2f7f3c9
turkeykittin <turkeykittin@gmail.com>
2024-11-28 22:26:50 -0500

    Added FIKA server side

---
+++ user/mods/fika-server/src/models/fika/routes/raid/dedicated/IStartDedicatedRequest.ts
@@ -0,0 +1,17 @@
+import { IBotSettings, ITimeAndWeatherSettings, IWavesSettings } from "@spt/models/eft/match/IRaidSettings";
+import { DateTime } from "@spt/models/enums/DateTime";
+import { PlayersSpawnPlace } from "@spt/models/enums/PlayersSpawnPlace";
+import { SideType } from "@spt/models/enums/SideType";
+
+export interface IStartDedicatedRequest {
+    expectedNumberOfPlayers: number;
+    time: DateTime;
+    locationId: string;
+    spawnPlace: PlayersSpawnPlace;
+    metabolismDisabled: boolean;
+    timeAndWeatherSettings: ITimeAndWeatherSettings;
+    botSettings: IBotSettings;
+    wavesSettings: IWavesSettings;
+    side: SideType;
+    customWeather: boolean;
+}
