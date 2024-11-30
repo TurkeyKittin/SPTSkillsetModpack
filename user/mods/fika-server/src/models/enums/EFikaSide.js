68f0e85bd74e4d9e2c75228f7189083ebdd24833
turkeykittin <turkeykittin@gmail.com>
2024-11-29 20:56:24 -0500

    Removed FIKA server

--- user/mods/fika-server/src/models/enums/EFikaSide.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EFikaSide = void 0;
-var EFikaSide;
-(function (EFikaSide) {
-    EFikaSide[EFikaSide["PMC"] = 0] = "PMC";
-    EFikaSide[EFikaSide["Savage"] = 1] = "Savage";
-})(EFikaSide || (exports.EFikaSide = EFikaSide = {}));
-//# sourceMappingURL=EFikaSide.js.map
