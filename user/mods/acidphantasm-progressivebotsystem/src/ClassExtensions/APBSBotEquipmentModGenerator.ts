3b80fa546bbd1725527146b1f2e060af02b661c5
SirSKillz <lruhland@purdue.edu>
2025-01-08 19:04:44 -0600

    Update apbs

--- user/mods/acidphantasm-progressivebotsystem/src/ClassExtensions/APBSBotEquipmentModGenerator.ts
+++ user/mods/acidphantasm-progressivebotsystem/src/ClassExtensions/APBSBotEquipmentModGenerator.ts
@@ -25,20 +25,23 @@ import { LocalisationService } from "@spt/services/LocalisationService";
 import { HashUtil } from "@spt/utils/HashUtil";
 import { RandomUtil } from "@spt/utils/RandomUtil";
 import { ICloner } from "@spt/utils/cloners/ICloner";
-
-import { BotEquipmentModGenerator } from "@spt/generators/BotEquipmentModGenerator";
-import { APBSEquipmentGetter } from "../Utils/APBSEquipmentGetter";
-import { APBSTierGetter } from "../Utils/APBSTierGetter";
-import { ModConfig } from "../Globals/ModConfig";
-import { RaidInformation } from "../Globals/RaidInformation";
-import { IChooseRandomCompatibleModResult } from "@spt/models/spt/bots/IChooseRandomCompatibleModResult";
-import { ModInformation } from "../Globals/ModInformation";
-import { APBSTester } from "../Utils/APBSTester";
 import { Money } from "@spt/models/enums/Money";
 import { IGenerateWeaponRequest } from "@spt/models/spt/bots/IGenerateWeaponRequest";
 import { IModToSpawnRequest } from "@spt/models/spt/bots/IModToSpawnRequest";
 import { BaseClasses } from "@spt/models/enums/BaseClasses";
 import { IEquipmentFilterDetails } from "@spt/models/spt/config/IBotConfig";
+import { BotEquipmentModGenerator } from "@spt/generators/BotEquipmentModGenerator";
+import { IChooseRandomCompatibleModResult } from "@spt/models/spt/bots/IChooseRandomCompatibleModResult";
+
+import { APBSEquipmentGetter } from "../Utils/APBSEquipmentGetter";
+import { APBSTierGetter } from "../Utils/APBSTierGetter";
+import { ModConfig } from "../Globals/ModConfig";
+import { RaidInformation } from "../Globals/RaidInformation";
+import { ModInformation } from "../Globals/ModInformation";
+import { APBSTester } from "../Utils/APBSTester";
+import { vanillaButtpads } from "../Globals/VanillaItemLists";
+import { APBSLogger } from "../Utils/APBSLogger";
+import { RealismHelper } from "../Helpers/RealismHelper";
 
 /** Handle profile related client events */
 @injectable()
@@ -69,7 +72,9 @@ export class APBSBotEquipmentModGenerator extends BotEquipmentModGenerator
         @inject("APBSTierGetter") protected apbsTierGetter: APBSTierGetter,
         @inject("RaidInformation") protected raidInformation: RaidInformation,
         @inject("ModInformation") protected modInformation: ModInformation,
-        @inject("APBSTester") protected apbsTester: APBSTester
+        @inject("APBSTester") protected apbsTester: APBSTester,
+        @inject("APBSLogger") protected apbsLogger: APBSLogger,
+        @inject("RealismHelper") protected realismHelper: RealismHelper
     )
     {
         super(logger, 
@@ -100,42 +105,12 @@ export class APBSBotEquipmentModGenerator extends BotEquipmentModGenerator
         const botRole = settings.botData.role;
         const tier = this.apbsTierGetter.getTierByLevel(settings.botData.level);
         const tieredModPool = this.apbsEquipmentGetter.getModsByBotRole(botRole, tier)
+        
+        let spawnChances = this.apbsEquipmentGetter.getSpawnChancesByBotRole(botRole, tier);
         let compatibleModsPool = tieredModPool[parentTemplate._id]
         let actualModPool = tieredModPool;
-        let spawnChances = this.apbsEquipmentGetter.getSpawnChancesByBotRole(botRole, tier);
 
-        // Roll weapon spawns (primary/secondary/holster) and generate a weapon for each roll that passed
-        if ((ModConfig.config.disableBossTierGeneration && (botRole.includes("boss") || botRole.includes("sectant") || botRole.includes("arena"))) || botRole == "bosslegion" || botRole == "bosspunisher")
-        {
-            spawnChances = settings.spawnChances;
-            compatibleModsPool = settings.modPool[parentTemplate._id];
-            actualModPool = settings.modPool;
-        }
-        if (ModConfig.config.disableBossFollowerTierGeneration && botRole.includes("follower"))
-        {
-            spawnChances = settings.spawnChances;
-            compatibleModsPool = settings.modPool[parentTemplate._id];
-            actualModPool = settings.modPool;
-        }
-        if (ModConfig.config.disableRaiderRogueTierGeneration && (botRole.includes("exusec") || botRole.includes("pmcbot")))
-        {
-            spawnChances = settings.spawnChances;
-            compatibleModsPool = settings.modPool[parentTemplate._id];
-            actualModPool = settings.modPool;
-        }
-        if (ModConfig.config.disablePMCTierGeneration && (botRole.includes("pmcusec") || botRole.includes("pmcbear")))
-        {
-            spawnChances = settings.spawnChances;
-            compatibleModsPool = settings.modPool[parentTemplate._id];
-            actualModPool = settings.modPool;
-        }
-        if (ModConfig.config.disableScavTierGeneration && (botRole.includes("assault") || botRole.includes("marksman")))
-        {
-            spawnChances = settings.spawnChances;
-            compatibleModsPool = settings.modPool[parentTemplate._id];
-            actualModPool = settings.modPool;
-        }
-        if (botRole.includes("infected") || botRole.includes("spirit") || botRole.includes("skier") || botRole.includes("peacemaker") || botRole.includes("gifter"))
+        if (!this.raidInformation.isBotEnabled(botRole))
         {
             spawnChances = settings.spawnChances;
             compatibleModsPool = settings.modPool[parentTemplate._id];
@@ -152,6 +127,11 @@ export class APBSBotEquipmentModGenerator extends BotEquipmentModGenerator
         for (const modSlotName in compatibleModsPool)
         {
             if (modSlotName === "mod_equipment_000" && this.raidInformation.nightTime) continue;
+
+            if (modSlotName === "mod_equipment" && this.realismHelper.gasMasks.includes(parentTemplate._id) && this.realismHelper.realismDetected == true)
+            {
+                forceSpawn = true;
+            }
 
             const itemSlotTemplate = this.getModItemSlotFromDb(modSlotName, parentTemplate);
             if (!itemSlotTemplate)
@@ -610,6 +590,14 @@ export class APBSBotEquipmentModGenerator extends BotEquipmentModGenerator
             {
                 continue;
             }
+
+            if (vanillaButtpads.includes(modToAddTemplate._id))
+            {
+                if (!this.randomUtil.getChance100(ModConfig.config.stockButtpadChance))
+                {
+                    continue;
+                }
+            }
             
             // If item is a mount for scopes, set scope chance to 100%, this helps fix empty mounts appearing on weapons
             if (this.modSlotCanHoldScope(modSlot, modToAddTemplate._parent)) 
@@ -633,18 +621,16 @@ export class APBSBotEquipmentModGenerator extends BotEquipmentModGenerator
                 }
             }
 
-            /*
-            if (!isPmc)
+            
+            if (ModConfig.config.forceChildrenMuzzle)
             {
                 // If picked item is muzzle adapter that can hold a child, adjust spawn chance
                 if (this.modSlotCanHoldMuzzleDevices(modSlot, modToAddTemplate._parent)) 
                 {
                     const muzzleSlots = ["mod_muzzle", "mod_muzzle_000", "mod_muzzle_001"];
-                    // Make chance of muzzle devices 95%
-                    this.adjustSlotSpawnChances(request.modSpawnChances, muzzleSlots, 95);
+                    this.adjustSlotSpawnChances(request.modSpawnChances, muzzleSlots, 100);
                 }
             }
-            */
             
             // If front/rear sight are to be added, set opposite to 100% chance
             if (this.modIsFrontOrRearSight(modSlot, modToAddTemplate._id)) 
